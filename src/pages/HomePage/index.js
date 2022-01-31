621bd1176df6428169ee5bb8bed2f60dc7cd6dd7
keithmacdonalddev <tenantbureau6@gmail.com>
2022-01-31 11:54:43 -0500

    homepage UI fix

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,16 +3,17 @@ import './index.css';
 
 const HomePage = () => {
 	return (
-		<div className='container'>
-			<Countdown />
-			<div className='tagline'>
-				Get on the VIP list to be the first to know when we launch!
+		<div className=''>
+			<div className='container'>
+				<Countdown />
+				<div className='tagline'>
+					Get on the VIP list to be the first to know when we launch!
+				</div>
 			</div>
 			<div className='email-input'>
 				<label htmlFor=''>Email:</label>
 				<input type='text' />
 			</div>
-			<div className='test'></div>
 		</div>
 	);
 };
