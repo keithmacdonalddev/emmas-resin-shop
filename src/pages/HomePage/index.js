4f1bea2c01509480471a63b5efe65b0e72be387e
keithmacdonalddev <tenantbureau6@gmail.com>
2022-01-31 02:34:21 -0500

    coming soon page

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,26 @@
+// import emmasLogo from '../../assets/logo.png';
+import './index.css';
+
+const HomePage = () => {
+	return (
+		<div className='container'>
+			<h2>Coming Soon</h2>
+			<h1>
+				Emma's <span>Resin Shop</span>
+				<div className='day'></div>
+				<div className='hour'></div>
+				<div className='minute'></div>
+				<div className='day'></div>
+			</h1>
+			<div className='tagline'>
+				Get on the VIP list to be the first to know when we launch!
+			</div>
+			<div className='email-input'>
+				<label htmlFor=''>Email:</label>
+				<input type='text' />
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
